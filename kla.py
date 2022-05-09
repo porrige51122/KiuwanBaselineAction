f3234b57fa514ea3cec3b0a8914a5bb1555def46
porrige51122 <aidan_smithers@btinternet.com>
2022-05-09 14:11:58 +0100

    Better format python and remove Spanish

--- kla.py
+++ kla.py
@@ -10,10 +10,6 @@ import sys
 import stat
 from pathlib import Path
 
-# print environment, the results should be there
-print("--------------------- ENVIRONMENT ------------------------")
-print (os.environ)
-print("--------------------- ENVIRONMENT ------------------------")
 
 # Params used in the call to the baseline analysis.
 # All the parameters set in the action are stored as environment variables with INPUT_ prefix
@@ -54,18 +50,11 @@ def getKLACmd(
 
 
 # Function to download and extract the Kiuwan Local Analyzer from kiuwan server
-# TODO: parametrize the URL to support Kiuwan on premises installations
-def downloadAndExtractKLA(tmp_dir=TMP_EXTRACTION_DIR, klaurl=KLA_URL):
-    print("Downloading KLA zip from ", klaurl, " at [", os.getcwd(), "]", "...")
-    resp = urllib.request.urlopen(klaurl)
+def downloadAndExtractKLA(tmp_dir=TMP_EXTRACTION_DIR, kla_url=KLA_URL):
+    print("Downloading KLA zip from ", kla_url, " to [", tmp_dir, "]")
+    resp = urllib.request.urlopen(kla_url)
     zipf = zipfile.ZipFile(io.BytesIO(resp.read()))
-    for item in zipf.namelist():
-        print("\tFile in zip: ", item)
-
-    print("Extracting zip to [", tmp_dir, "]", "...")
-    # Luis: para probar crear dirs
     Path(tmp_dir).mkdir(parents=True, exist_ok=True)
-
     zipf.extractall(tmp_dir)
 
 
@@ -85,33 +74,33 @@ def getBLAnalysisResultsURL(
 ):
     apicall = "https://api.kiuwan.com"
     if not PARAM_KLA_BASEURL:
-        apicall = PARAM_KLA_BASEURL + "/saas/rest/v1"
+        apicall = f"{PARAM_KLA_BASEURL}/saas/rest/v1"
 
     apicall = apicall + "/apps/analysis/" + a_c
-    print("Calling REST API [", apicall, "] ...")
+    print(f"Calling REST API [ {apicall} ]")
 
     kla_password = kla_password.replace("\\", "")
-    authString = base64.encodebytes(('%s:%s' % (kla_user,kla_password)).encode()).decode().strip()
+    authString = (
+        base64.encodebytes((f"{kla_user}:{kla_password}").encode()).decode().strip()
+    )
 
     if "domain-id" in advanced:
         posDomain = advanced.find("domain-id")
-        value_domain_id = advanced[posDomain + 10 :]  ##remove domain-id word
+        value_domain_id = advanced[posDomain + 10 :]
         posWhitespace = value_domain_id.find(" ")
         if posWhitespace != -1:
             value_domain_id = value_domain_id[:posWhitespace]
         my_headers = {
-            "Authorization": "Basic {}".format(authString),
+            "Authorization": f"Basic {authString}",
             "X-KW-CORPORATE-DOMAIN-ID": value_domain_id,
         }
     else:
-        my_headers = {"Authorization": "Basic {}".format(authString)}
+        my_headers = {"Authorization": f"Basic {authString}"}
     response = requests.get(url=apicall, headers=my_headers)
-    # response = requests.get(apicall, auth=requests.auth.HTTPBasicAuth(kla_user, kla_password))
 
     print(response)
-    print("Contenido", response.content)
+    print("Response Contents:", response.content)
     jdata = json.loads(response.content)
-    # print ('URL del analisis: ' , jdata['analysisURL'])
     return jdata["analysisURL"]
 
 
@@ -119,7 +108,6 @@ def getBLAnalysisResultsURL(
 def executeKLA(cmd):
     print("Executing [", cmd, "] ...")
     pipe = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-    # (output, err) = pipe.communicate()
     output_text = ""
     try:
         nextline = pipe.stdout.readline()
@@ -129,10 +117,7 @@ def executeKLA(cmd):
             sys.stdout.flush()
             nextline = pipe.stdout.readline()
     except KeyboardInterrupt:
-        print("Keyboard interrupt... why??")
         return output_text, pipe.returncode
-
-    # return_code = pipe.wait()
     return output_text, pipe.returncode
 
 
@@ -145,72 +130,16 @@ kla_bl_cmd = getKLACmd(tmp_dir=TMP_EXTRACTION_DIR)
 
 # Execute CLA KLI and set results as outputs
 output, rc = executeKLA(kla_bl_cmd)
-print("::set-output name=result::{}".format(rc))
-print("{}{}".format("KLA return code: ", rc))
+print(f"::set-output name=result::{rc}")
+print(f"KLA return code: {rc}")
 if rc == 0:
     analysis_code = getBLAnalysisCodeFromKLAOutput(output)
-    print("Analysis code [", analysis_code, "]")
+    print(f"Analysis code [ {analysis_code} ]")
     url_analysis = getBLAnalysisResultsURL(analysis_code)
-    # print traces and set output parameters...
-    print("URL del analisis: ", url_analysis)
-    print("::set-output name=analysisurl::{}".format(url_analysis))
+    print("Analysis URL: ", url_analysis)
+    print(f"::set-output name=analysisurl::{url_analysis}")
     print("::set-output name=message::Analysis successful.")
-elif rc == 1:
-    print("::set-output name=message::Analyzer execution error.")
-elif rc == 10:
-    print("::set-output name=message::Audit overall result = FAIL.")
-elif rc == 11:
-    print("::set-output name=message::Invalid analysis configuration.")
-elif rc == 12:
-    print(
-        "::set-output name=message::The downloaded model does not support any of the discovered languages."
-    )
-elif rc == 13:
-    print("::set-output name=message::Timeout waiting for analysis results.")
-elif rc == 14:
-    print("::set-output name=message::Analysis finished with an error in Kiuwan.")
-elif rc == 15:
-    print("::set-output name=message::Timeout: killed the subprocess.")
-elif rc == 16:
-    print(
-        "::set-output name=message::Baseline analysis not permitted for current user."
-    )
-elif rc == 17:
-    print(
-        "::set-output name=message::Delivery analysis not permitted for current user."
-    )
-elif rc == 18:
-    print("::set-output name=message::No analyzable extensions found.")
-elif rc == 19:
-    print("::set-output name=message::Error checking license.")
-elif rc == 21:
-    print("::set-output name=message::Invalid CLI parameter	.")
-elif rc == 22:
-    print("::set-output name=message::Access denied.")
-elif rc == 23:
-    print("::set-output name=message::Bad Credentials.")
-elif rc == 24:
-    print("::set-output name=message::Application Not Found.")
-elif rc == 25:
-    print("::set-output name=message::Limit Exceeded for Calls to Kiuwan API.")
-elif rc == 26:
-    print("::set-output name=message::Quota Limit Reached	.")
-elif rc == 27:
-    print("::set-output name=message::Analysis Not Found.")
-elif rc == 28:
-    print("::set-output name=message::Application already exists.")
-elif rc == 30:
-    # we should not be here, this is a baseline
-    print(
-        "::set-output name=message::Delivery analysis not permitted: baseline analysis not found."
-    )
-elif rc == 31:
-    print("::set-output name=message::No engine available.")
-elif rc == 32:
-    print("::set-output name=message::Unexpected error	.")
-elif rc == 33:
-    print("::set-output name=message::Out of Memory.")
-elif rc == 34:
-    print("::set-output name=message::JVM Error	.")
 else:
-    print("::set-output name=message::No error message found.")
+    print(
+        f"::set-output name=message::Error code {rc}, please go to:\n https://www.kiuwan.com/docs/display/K5/Analysis+Error+Code+Reference"
+    )
